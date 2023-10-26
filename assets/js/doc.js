dde7a525a9141376e184664d38a12e3228ebeadf
Priyanka-390 <psr82220@gmail.com>
2023-10-26 15:31:21 +0530

    changes in navbar

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,8 +1,10 @@
 // nav-bar
 function openNav() {
+    document.getElementById("menubtn-icon").classList.toggle("circle-menu")
     document.getElementById("navbar").classList.toggle("end-0");
     document.body.classList.toggle("overflow_hidden");
     document.getElementById("menubtn-icon").classList.toggle("cross")
+
 }
 
 // back to top-------
