9f87824537e505e344289dc306f86dd2bbdeac5b
Priyanka-390 <psr82220@gmail.com>
2023-11-01 13:59:11 +0530

    buttons modify

---
+++ app.js
@@ -0,0 +1,16 @@
+// if (4 === 4) {
+//     console.log("success");
+// } else {
+//     console.log("error");
+// }
+// let exp = 1
+// switch (exp) {
+//     case 11:
+//         console.log("1");
+//         break;
+//     case 10:
+//         console.log("2");
+//         break;
+//     default:
+//         console.log("default");
+// }
